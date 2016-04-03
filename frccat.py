a4e1642456cc9b5801ceec8e68eb990a7981ff37
Randy Carlson <fcarlson@ieee.org>
2016-04-03 13:31:18 -0400

    Update frccat.py

--- frccat.py
+++ frccat.py
@@ -119,7 +119,7 @@ def server_loop():
         global target
         
         if not len(target):
-                target=0.0.0.0
+                target="0.0.0.0"
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((target,port))
         server.listen(5)
